29387c628fb1cf79cc578d979670e07a731dd7e3
Alexander Schelchere <ascellie@gmail.com>
2019-08-16 18:20:02 -0400

    user_controller: index, show, create, authenticate

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,28 @@
 class UsersController < ApplicationController
   attr_accessor :passsword
 
+  def index 
+    @users = User.all
+    render json: @users
+  end
+
+  def show
+    @user = User.find(params[:id])
+    # byebug
+    render json: @user
+  end
+  
+  def create 
+    get_user_params
+    # byebug
+    @user = User.create(@user_params)
+    if @user.valid?
+      render json: { user: UserSerializer.new(@user) }, status: :created
+    else
+      render json: { error: @user.errors.full_messages }, status: :not_acceptable
+    end
+  end
+  
   def authenticate(plaintext_passwprd)
     if BCrypt::Password.new(self.password_digest) == plaintext_passwprd
       self 
@@ -9,17 +31,12 @@ class UsersController < ApplicationController
     end
   end
 
-  def create 
-    @user = User.create(user_params)
-    if user.valid?
-      render json: { user: UserSerializer.new(@user) }, status: :created
-    else
-      render json: { error: params.errors.full_messages }, status: :not_acceptable
-    end
-  end
-
   private
-  def user_params
-    params.require(:user).permit(:name, :username, :email, :password)
+  def get_user_params
+    params.permit(:name, :username, :email, :password)
+    @user_params = { name: params[:name], username: params[:username], email: params[:email], password: params[:password]}
   end
+  # def user_params
+  #  params.require(:user).permit(:name, :username, :email, :password)
+  # end
 end
