85d983b3a03ac986e863b9d2edfc1a5494d3df7c
Richard Li <richardli125@gmail.com>
2019-08-19 11:49:05 -0400

    updated

--- db/schema.rb
+++ db/schema.rb
@@ -21,7 +21,7 @@ ActiveRecord::Schema.define(version: 2019_08_16_190410) do
     t.string "email"
     t.string "password_digest"
     t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "updated_at", null: falsex
   end
 
 end
