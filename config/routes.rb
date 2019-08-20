0ab8801cd93429c26f12cfc5376829ac6f36f18e
Alexander Schelchere <ascellie@gmail.com>
2019-08-20 12:21:42 -0400

    config: new routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
-  resources :tasks
-  resources :lists
-  resources :projects
   get '/persist', to: 'tokens#persist'
-
-  resources :users
   
   resources :tokens, only: [:create]
+  resources :users  
+  resources :projects
+  resources :lists
+  resources :tasks
 end
